c92e2133cc98a4698c1db618eb43f153f884ac9e
yoshiki <yoshix47@gmail.com>
2020-07-04 09:47:00 +0900

    Create tweets_list_view

--- tweets/views.py
+++ tweets/views.py
@@ -10,9 +10,22 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Android
+    return json data
+    """
+    qs = Tweet.objects.all()
+    tweet_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweet_list
+    }
+    return JsonResponse(data)
+
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
-
     REST API VIEW
     Consume by JavaScript or Swift/Java/iOS/Android
     return json data
