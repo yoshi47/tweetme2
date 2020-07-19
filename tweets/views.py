db3870f58e1045e17887c2159e8700a585e771b5
yoshiki <yoshix47@gmail.com>
2020-07-19 17:05:42 +0900

    31 - Handling Form Errors

--- tweets/views.py
+++ tweets/views.py
@@ -17,6 +17,7 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_create_view(request, *args, **kwargs):
+    print(abc)
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
     if form.is_valid():
@@ -27,6 +28,9 @@ def tweet_create_view(request, *args, **kwargs):
         if next_url is not None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
     return render(request, 'components/form.html', context={"form": form})
 
 
