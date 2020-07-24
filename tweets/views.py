26848e35bcd78c9e45d17614a0dadc64072af4bc
yoshiki <yoshix47@gmail.com>
2020-07-24 20:46:12 +0900

    41 - Delete Tweet API View

--- tweets/views.py
+++ tweets/views.py
@@ -46,7 +46,21 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
         return Response({}, status=404)
     obj = qs.first()
     serializer = TweetSerializer(obj)
-    return Response(serializer.data)
+    return Response(serializer.data, status=200)
+
+
+@api_view(["DELETE", "POST"])
+@permission_classes([IsAuthenticated])
+def tweet_delete_view(request, tweet_id, *args, **kwargs):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    qs = qs.filter(user=request.user)
+    if not qs.exists():
+        return Response({"message": "You cannot delete this tweet"}, status=401)
+    obj = qs.first()
+    obj.delete()
+    return Response({"message": "Tweet removed"}, status=200)
 
 
 def tweet_create_view_pure_django(request, *args, **kwargs):
