d95f528d9d1c73a69b114a9e6261bfcb74568c9f
yoshiki <yoshix47@gmail.com>
2020-07-31 15:18:40 +0900

    49. Internal App Urls

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from tweets.views import (
     home_view,
     tweet_action_view,
@@ -10,10 +10,9 @@ from tweets.views import (
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home_view),
-    path('create-tweet', tweet_create_view),
-    path('tweets', tweet_list_view),
+    path('', tweet_list_view),
+    path('action/', tweet_action_view),
+    path('create-tweet/', tweet_create_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
-    path('api/tweets/', include('tweets.urls'))
+    path('<int:tweet_id>/delete', tweet_delete_view),
 ]
