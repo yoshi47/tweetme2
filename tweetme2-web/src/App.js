6115de3e6f9a44a572e33af8ab529e44a938a72e
yoshiki <yoshix47@gmail.com>
2020-08-11 20:04:40 +0900

    53. Understanding Functional Components

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [tweets, setTweets] = useState([])
+  useEffect(() => {
+    // do my lookup
+    const tweetItems = [{"content": 123}, {"content": "Hello world"}]
+    setTweets(tweetItems)
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>
+            {tweets.map((tweet, index) => {
+              return <li>{tweet.content}</li>
+            })}
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
