2c1763f232f67c71f0413c015ee6aad876f9abde
yoshiki <yoshix47@gmail.com>
2020-08-28 09:12:36 +0900

    84. Authentication & Registration

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -4,6 +4,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from accounts.views import (
+    login_view,
+    logout_view,
+    register_view
+)
 
 from tweets.views import (
     tweets_detail_view,
@@ -14,6 +19,9 @@ from tweets.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', tweets_list_view),
+    path('login/', login_view),
+    path('logout/', logout_view),
+    path('register/', register_view),
     path('<int:tweet_id>', tweets_detail_view),
     path('profile/<str:username>', tweets_profile_view),
     path('api/tweets/', include('tweets.api.urls')),
