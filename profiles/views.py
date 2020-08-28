fa8c97d54c77fc0fa3e1f28e4ca7bb91cc628211
yoshiki <yoshix47@gmail.com>
2020-08-28 18:07:12 +0900

    85. User Profiles

---
+++ profiles/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def profile_detail_view(request, username, *args, **kwargs):
+
+    return render(request, "profiles/detail.html", {"username": username})
