4b6f0ddb633f403130b10bcc6723ea63d27df920
yoshiki <yoshix47@gmail.com>
2020-08-30 16:26:36 +0900

    91. Follow Button Logic and Endpoint

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('<int:tweet_id>', tweets_detail_view),
     re_path(r'profiles?/', include('profiles.urls')),
     path('api/tweets/', include('tweets.api.urls')),
+    re_path(r'api/profiles?/', include('profiles.api.urls')),
 ]
 
 if settings.DEBUG:
