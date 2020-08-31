85b56a3760ecbc00a43af86a9dc1a163277dd1f4
yoshiki <yoshix47@gmail.com>
2020-08-31 15:15:22 +0900

    92. Profile Following Unit Test Cases

--- profiles/models.py
+++ profiles/models.py
@@ -22,7 +22,7 @@ class Profile(models.Model):
     """
     project_obj = Profile.objects.first()
     project_obj.followers.all() -> All users following this profile
-    user.following.all() -> all users I follow
+    user.following.all() -> All users I follow
     """
 
 
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,6 +1,6 @@
-from django.test import TestCase
-
 from django.contrib.auth import get_user_model
+from django.test import TestCase
+from rest_framework.test import APIClient
 
 from .models import Profile
 
@@ -13,6 +13,11 @@ class ProfileTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='cfe', password='somepassword')
         self.userb = User.objects.create_user(username='cfe-2', password='somepassword2')
+
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password='somepassword')
+        return client
 
     def test_profile_created_via_signal(self):
         qs = Profile.objects.all()
@@ -27,3 +32,37 @@ class ProfileTestCase(TestCase):
         first_user_following_no_one = first.following.all()
         self.assertTrue(qs.exists())
         self.assertFalse(first_user_following_no_one.exists())
+
+    def test_follow_api_endpoint(self):
+        client = self.get_client()
+        response = client.post(
+            f"/api/profiles/{self.userb.username}/follow",
+            {"action": "follow"}
+        )
+        r_data = response.json()
+        count = r_data.get("count")
+        self.assertEqual(count, 1)
+
+    def test_unfollow_api_endpoint(self):
+        first = self.user
+        second = self.userb
+        first.profile.followers.add(second)
+
+        client = self.get_client()
+        response = client.post(
+            f"/api/profiles/{self.userb.username}/follow",
+            {"action": "unfollow"}
+        )
+        r_data = response.json()
+        count = r_data.get("count")
+        self.assertEqual(count, 0)
+
+    def test_cannot_follow_api_endpoint(self):
+        client = self.get_client()
+        response = client.post(
+            f"/api/profiles/{self.user.username}/follow",
+            {"action": "follow"}
+        )
+        r_data = response.json()
+        count = r_data.get("count")
+        self.assertEqual(count, 0)
